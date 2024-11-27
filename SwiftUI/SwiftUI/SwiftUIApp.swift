8eb463e5fb20fc46383ed2e1901ff1acfe027de5
jihoon <leejh080628@naver.com>
2024-11-27 11:07:36 +0900

    setting :: 프로젝트 세팅

---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by 이지훈 on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
